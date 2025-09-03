98f18c22e6bcc93d6d1892ab0799cf44fd9c77d1
joaoprestes0j-ui <joao.prestes0j@gmail.com>
2025-09-03 16:04:59 -0300

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>🚀 Deploy funcionando!</h1>
+      <p>Se você está vendo esta página, o build deu certo 🎉</p>
+    </main>
+  );
+}
